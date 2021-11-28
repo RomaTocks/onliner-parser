f0f34f60a4c857b52059b0b7122c1baff57d7f77
Roman <56370973+RomaTocks@users.noreply.github.com>
2021-11-29 01:13:44 +0300

    Added chassis model.

---
+++ src/main/java/com/onliner/tocks/model/Chassis.java
@@ -0,0 +1,17 @@
+package com.onliner.tocks.model;
+
+import com.onliner.tocks.model.additional.ChassisAdditionalInformation;
+import com.onliner.tocks.parsing.common.Product;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = "chassis")
+@ToString(callSuper = true)
+public class Chassis extends Product
+{
+    private ChassisAdditionalInformation additional;
+}
