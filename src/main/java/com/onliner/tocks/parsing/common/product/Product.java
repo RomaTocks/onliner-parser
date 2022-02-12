a21ebee3b066638265d42c88df92bed9d18a395e
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-02-13 02:32:15 +0300

    Added manufacturer field to product's.

--- src/main/java/com/onliner/tocks/parsing/common/product/Product.java
+++ src/main/java/com/onliner/tocks/parsing/common/product/Product.java
@@ -1,6 +1,7 @@
 package com.onliner.tocks.parsing.common.product;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.onliner.tocks.parsing.common.price.Price;
 import com.onliner.tocks.parsing.common.price.Prices;
 import com.onliner.tocks.parsing.common.sellers.Position;
 import lombok.Data;
@@ -15,6 +16,7 @@ public class Product {
     protected String id;
     protected String key;
     protected String name;
+    protected Manufacturer manufacturer;
     protected String description;
     protected Prices prices;
     protected String html_url;
@@ -27,7 +29,17 @@ public class Product {
         this.setDescription(product.getDescription());
         this.setHtml_url(product.getHtml_url());
         this.setName(product.getName());
-        this.setPrices(product.getPrices());
+        this.setPrices(priceWithValues(product.getPrices()));
         this.setPositions(product.getPositions());
+        this.setManufacturer(product.getManufacturer());
+    }
+    private Prices priceWithValues(Prices prices) {
+        Price minimalPrice = prices.getPrice_min();
+        Price maxPrice = prices.getPrice_max();
+        minimalPrice.setValue(Double.parseDouble(minimalPrice.getAmount()));
+        maxPrice.setValue(Double.parseDouble(maxPrice.getAmount()));
+        prices.setPrice_min(minimalPrice);
+        prices.setPrice_max(maxPrice);
+        return prices;
     }
 }
