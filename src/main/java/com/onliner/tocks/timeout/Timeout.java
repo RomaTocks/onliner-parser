45462cb05009ada1b21f9fb8650da5b913c23455
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-02-18 01:20:47 +0300

    Added fixed timeout function to parsing process.

--- src/main/java/com/onliner/tocks/timeout/Timeout.java
+++ src/main/java/com/onliner/tocks/timeout/Timeout.java
@@ -9,23 +9,28 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class Timeout
 {
+    public static void getFixedTimeout(Integer time, TimeUnit unit) {
+        System.out.println();
+        log.info("Fixed timeout:" + time + " " + unit.toString());
+        System.out.println();
+        getSleep(time, unit);
+    }
     public static void getTimeout() {
         int random = (int) (Math.random()*2) + 1;
         System.out.println();
         log.info("Try to avoid ban from onliner.by : Waiting " + random + " seconds.");
         System.out.println();
-        try {
-            TimeUnit.SECONDS.sleep(random);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        getSleep(random, TimeUnit.SECONDS);
     }
     public static void getChapterTimeout() {
         System.out.println();
         log.info("Try to avoid ban from onliner.by : Next chapter, waiting " + 1 + " minute.");
         System.out.println();
+        getSleep(1, TimeUnit.MINUTES);
+    }
+    private static void getSleep(Integer time, TimeUnit unit) {
         try {
-            TimeUnit.MINUTES.sleep(1);
+            unit.sleep(time);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
