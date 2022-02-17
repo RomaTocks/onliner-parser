3bc6e26a31afc5ff6fa9670fa2abf4e69195be63
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-02-18 01:26:47 +0300

    Added converter of string numeric values.

---
+++ src/main/java/com/onliner/tocks/parsing/converter/Converter.java
@@ -0,0 +1,34 @@
+package com.onliner.tocks.parsing.converter;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Converter
+{
+    private static final String INTEGER_PATTERN = "\\d+";
+    private static final String DOUBLE_PATTERN = "\\d+\\.\\d+";
+
+    public static Integer getIntegersFromString(String string) {
+        Pattern pattern = Pattern.compile(INTEGER_PATTERN);
+        return Integer.valueOf(prepareString(pattern, string));
+    }
+    public static Double getDoubleFromString(String string) {
+        Pattern pattern = Pattern.compile(DOUBLE_PATTERN);
+        String preparedString = prepareString(pattern, string);
+        if(preparedString.isEmpty()) {
+            pattern = Pattern.compile(INTEGER_PATTERN);
+            String newPreparedString = prepareString(pattern, string);
+            return Double.valueOf(newPreparedString);
+        }
+        return Double.valueOf(prepareString(pattern,string));
+    }
+    private static String prepareString(Pattern pattern, String string) {
+        Matcher matcher = pattern.matcher(string);
+        StringBuilder value = new StringBuilder();
+        while (matcher.find())
+        {
+            value.append(string, matcher.start(), matcher.end());
+        }
+        return value.toString();
+    }
+}
