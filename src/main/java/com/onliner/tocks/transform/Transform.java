2a758f9ea65786102631b4a20deacfb769982bb7
Roman <56370973+RomaTocks@users.noreply.github.com>
2022-03-03 03:43:51 +0300

    Added getting numeric values from additional.

--- src/main/java/com/onliner/tocks/transform/Transform.java
+++ src/main/java/com/onliner/tocks/transform/Transform.java
@@ -1,8 +1,11 @@
 package com.onliner.tocks.transform;
 
 import com.onliner.tocks.model.*;
+import com.onliner.tocks.model.additional.*;
+import com.onliner.tocks.model.additional.values.*;
 import com.onliner.tocks.parsing.common.ProductsEnum;
 import com.onliner.tocks.parsing.common.product.Product;
+import com.onliner.tocks.parsing.converter.Converter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -113,8 +116,120 @@ public class Transform
             }
         }
     }
-    public static Integer getNumbersInString(String string) {
-        String numbers = string.replaceAll("[^0-9]", "");
-        return Integer.parseInt(numbers);
+    public static List<? extends Product> getValuesFromAdditional(List<? extends Product> list, ProductsEnum productsEnum) {
+        switch (productsEnum) {
+
+            case PROCESSORS: {
+                List<CPU> cpus = (List<CPU>) list;
+                cpus.forEach(cpu -> {
+                    CPUAdditionalValues cpuAdditionalValues = new CPUAdditionalValues();
+                    CPUAdditionalInformation additional = cpu.getAdditional();
+                    cpuAdditionalValues.setBirthday(checkStringInteger(additional.getBirthday()));
+                    cpuAdditionalValues.setCores(checkStringInteger(additional.getCores()));
+                    cpuAdditionalValues.setCpuRamChannel(checkStringInteger(additional.getCpuRamChannel()));
+                    cpuAdditionalValues.setStreams(checkStringInteger(additional.getStreams()));
+                    cpuAdditionalValues.setTdp(checkStringInteger(additional.getTdp()));
+                    cpuAdditionalValues.setStreams(checkStringInteger(additional.getStreams()));
+                    cpuAdditionalValues.setBaseFrequency(checkStringDouble(additional.getBaseFrequency()));
+                    cpuAdditionalValues.setTurboFrequency(checkStringDouble(additional.getTurboFrequency()));
+                    cpuAdditionalValues.setRamFrequency(checkStringInteger(additional.getRamFrequency()));
+                    cpu.setValues(cpuAdditionalValues);
+                });
+                return cpus;
+            }
+            case GRAPHIC_CARDS:
+                List<GraphicCard> graphicCards = (List<GraphicCard>) list;
+                graphicCards.forEach(graphicCard -> {
+                    GraphicCardAdditionalValues graphicCardAdditionalValues = new GraphicCardAdditionalValues();
+                    GraphicCardAdditionalInformation additional = graphicCard.getAdditional();
+                    graphicCardAdditionalValues.setHeight(checkStringInteger(additional.getHeight()));
+                    graphicCardAdditionalValues.setLength(checkStringInteger(additional.getLength()));
+                    graphicCardAdditionalValues.setVideoMemory(checkStringInteger(additional.getVideoMemory()));
+                    graphicCardAdditionalValues.setPSU(checkStringInteger(additional.getPSU()));
+                    graphicCard.setValues(graphicCardAdditionalValues);
+                });
+                return graphicCards;
+            case RAM:
+                List<Ram> rams = (List<Ram>) list;
+                rams.forEach(ram -> {
+                    RamAdditionalValues ramAdditionalValues = new RamAdditionalValues();
+                    RamAdditionalInformation additional = ram.getAdditional();
+                    ramAdditionalValues.setFrequency(checkStringInteger(additional.getFrequency()));
+                    ramAdditionalValues.setKit(checkStringInteger(additional.getKit()));
+                    ram.setValues(ramAdditionalValues);
+                });
+                return rams;
+            case SSD:
+                List<SSD> ssds = (List<SSD>) list;
+                ssds.forEach(ssd -> {
+                    SSDAdditionalValues ssdAdditionalValues = new SSDAdditionalValues();
+                    SSDAdditionalInformation additional = ssd.getAdditional();
+                    ssdAdditionalValues.setVolume(checkStringInteger(additional.getVolume()));
+                    ssd.setValues(ssdAdditionalValues);
+                });
+                return ssds;
+            case HDD:
+                List<HDD> hdds = (List<HDD>) list;
+                hdds.forEach(hdd -> {
+                    HDDAdditionalValues hddAdditionalValues = new HDDAdditionalValues();
+                    HDDAdditionalInformation additional = hdd.getAdditional();
+                    hddAdditionalValues.setVolume(checkStringInteger(additional.getVolume()));
+                    hddAdditionalValues.setDepth(checkStringInteger(additional.getDepth()));
+                    hddAdditionalValues.setPowerUsage(checkStringDouble(additional.getPowerUsage()));
+                    hdd.setValues(hddAdditionalValues);
+                });
+                return hdds;
+            case COOLING:
+                List<Fan> fans = (List<Fan>) list;
+                fans.forEach(fan -> {
+                    FanAdditionalValues fanAdditionalValues = new FanAdditionalValues();
+                    FanAdditionalInformation additional = fan.getAdditional();
+                    fanAdditionalValues.setDiameterFan(checkStringInteger(additional.getDiameterFan()));
+                    fanAdditionalValues.setDispelPower(checkStringInteger(additional.getDispelPower()));
+                    fanAdditionalValues.setHeight(checkStringInteger(additional.getHeight()));
+                    fan.setValues(fanAdditionalValues);
+                });
+                return fans;
+            case PSU:
+                List<PSU> psus = (List<PSU>) list;
+                psus.forEach(psu -> {
+                    PSUAdditionalValues psuAdditionalValues = new PSUAdditionalValues();
+                    PSUAdditionalInformation additional = psu.getAdditional();
+                    psuAdditionalValues.setPower(checkStringInteger(additional.getPower()));
+                    psuAdditionalValues.setHeight(checkStringInteger(additional.getHeight()));
+                    psuAdditionalValues.setWidth(checkStringInteger(additional.getWidth()));
+                });
+            case MOTHERBOARDS:
+                List<Motherboard> motherboards = (List<Motherboard>) list;
+                motherboards.forEach(motherboard -> {
+                    MotherboardAdditionalValues values = new MotherboardAdditionalValues();
+                    MotherboardAdditionalInformation additional = motherboard.getAdditional();
+                    values.setChassisFan(checkStringInteger(additional.getChassisFan()));
+                });
+            case CHASSIS:
+                List<Chassis> chassises = (List<Chassis>) list;
+                chassises.forEach(chassis -> {
+                    ChassisAdditionalValues values = new ChassisAdditionalValues();
+                    ChassisAdditionalInformation additional = chassis.getAdditional();
+                    values.setDepth(checkStringInteger(additional.getDepth()));
+                    values.setHeight(checkStringInteger(additional.getHeight()));
+                    values.setFanKit(checkStringInteger(additional.getFanKit()));
+                    values.setWeight(checkStringDouble(additional.getWeight()));
+                    values.setMaxCPUCoolingSystemHeight(checkStringInteger(additional.getMaxCPUCoolingSystemHeight()));
+                    values.setMaxGPULength(checkStringInteger(additional.getMaxGPULength()));
+                    values.setMaxPSULength(checkStringInteger(additional.getMaxPSULength()));
+                    chassis.setValues(values);
+                });
+                return chassises;
+            default: {
+                return null;
+            }
+        }
+    }
+    private static Integer checkStringInteger(String string) {
+        return string != null ? Converter.getIntegersFromString(string) : null;
+    }
+    private static Double checkStringDouble(String string) {
+        return string != null ? Converter.getDoubleFromString(string) : null;
     }
 }
